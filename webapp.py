ab2b7b346cc1e90285d8b2e8c0bef4da3dbdf5a0
Your NameE-Perko <edward.p.perko@gmail.com>
2024-10-17 14:18:48 -0700

    add

--- webapp.py
+++ webapp.py
@@ -1,10 +1,20 @@
 from flask import Flask, url_for, render_template
+from markupsafe import Markup
+from datetime import datetime
 
-app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+import os
+import json
+
+app = Flask(__name__)
 
 @app.route("/")
 def render_main():
     return render_template('home.html')
+
+def is_localhost():
+    root_url = request.url_root
+    developer_url = 'http://127.0.0.1:5000/'
+    return root_url == developer_url
     
 if __name__=="__main__":
     app.run(debug=False)
