a6195b82da384b723a2fca8bdb192e86f75d0471
E-Perko <edward.p.perko@gmail.com>
2024-12-01 15:20:25 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -46,7 +46,7 @@ def console_game_totals(games):
             consoles[console] = 1
     code = ""
     for console, video_games in consoles.items():
-        code = code + Markup("{ x: '" + str(console) + "', y: " + str(video_games) + " },")
+        code = code + Markup("{ x: '" + str(console) + "', y: " + str(video_games) + " }, ")
     code = code[:-1]
     return code
     
