c287102f3802c9c44d70af5d44f585cecdb4eb87
E-Perko <edward.p.perko@gmail.com>
2024-12-01 23:02:12 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,8 +17,14 @@ def render_recommender():
         games = json.load(video_games_data)
     if 'genre' in request.args:
         genre = request.args['genre']
-        return render_template('recommenderdata.html', options=get_genre_options(games), genre=genre)
+        return render_template('recommenderdata.html', options=get_genre_options(games), genre_list=genre)
     return render_template('recommender.html', options=get_genre_options(games))
+
+@app.route("/consolechart")
+def render_consolechart():
+    with open('video_games.json') as video_games_data:
+        games = json.load(video_games_data)
+    return render_template('consolechart.html', points=console_game_totals(games))
 
 def get_genre_options(games):
     genres = []
@@ -29,12 +35,6 @@ def get_genre_options(games):
             genres.append(genre)
             options += Markup("<option value=\"" + str(genre) + "\">" + str(genre) + "</option>")
     return options    
-
-@app.route("/consolechart")
-def render_consolechart():
-    with open('video_games.json') as video_games_data:
-        games = json.load(video_games_data)
-    return render_template('consolechart.html', points=console_game_totals(games))
 
 def console_game_totals(games):
     consoles = {}
