3a9c5e1b91ec1ef112bca0dc1ff4bd7dd6ce17de
E-Perko <edward.p.perko@gmail.com>
2024-12-01 19:26:13 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -19,6 +19,10 @@ def render_recommender():
         genre = request.args['genre']
         return render_template('recommenderdata.html', options=get_genre_options(games), genre=genre)
     return render_template('recommender.html', options=get_genre_options(games))
+
+@app.route('/recommenderdata')
+def render_recommenderdata():
+    return render_template('recommenderdata.html')
     
 def get_genre_options(games):
     genres = []
